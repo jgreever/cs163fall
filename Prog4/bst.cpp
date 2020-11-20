f9dd80ba5e00bdd6168869fd42a5596177db3869
Justin Greever <jgreever@gmail.com>
2020-11-19 18:09:11 -0800

    Created empty files that will be used for Program 4 and added comment info at the top of them

---
+++ Prog4/bst.cpp
@@ -0,0 +1,11 @@
+/* bst.cpp
+ *
+ * Justin Greever
+ * CS163 - Program 4
+ * 11/19/2020
+ *
+ * Media Management (using BSTs)
+ *
+ */
+
+
---
+++ Prog4/table.hpp
@@ -0,0 +1,11 @@
+/* table.hpp
+ *
+ * Justin Greever
+ * CS163 - Program 4
+ * 11/19/2020
+ *
+ * Media Management (using BSTs)
+ *
+ */
+
+
